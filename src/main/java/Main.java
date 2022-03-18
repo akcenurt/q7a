cc2c745b190e6049a4dfba942436dec8072e7f61
akcenurt <98740756+akcenurt@users.noreply.github.com>
2022-03-18 11:11:00 +0100

    Add java maven proj

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public int divisionWithRemainder (int a, int b) {
+        return a % b;
+    }
+
+
+//    public static void main(String[] args) {
+//        int a = 8;
+//        int b = 5;
+//        System.out.println(a % b);
+//    }
+}
